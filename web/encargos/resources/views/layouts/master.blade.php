b3129885353297995b4e5ed73c9cf1a5a67917da
Juan Gallardo <juangallardovergara17@gmail.com>
2021-08-01 18:51:44 -0400

    Generado el control de acceso

--- web/encargos/resources/views/layouts/master.blade.php
+++ web/encargos/resources/views/layouts/master.blade.php
@@ -35,3 +35,9 @@
         <main class = "container-fluid">
             @yield("contenido")
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        @yield("javascript")
+      </body>
+    </html>
